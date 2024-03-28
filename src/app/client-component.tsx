00fb25fa3c5893a89ca5747a5bc42d202d0c62e4
Daniel Ivanov <daniel.ivanov@me.com>
2024-03-28 22:49:34 +0100

    - faking client component to compare on deployed build;

--- src/app/client-component.tsx
+++ src/app/client-component.tsx
@@ -2,7 +2,7 @@
 
 export const ClientComponent: React.FC = () => {
   return (
-    <div className="bg-neutral-800 w-52 text-white text-center text-xs sm:text-base rounded-md shadow-md p-2 md:p-5 z-10">
+    <div className="csr-component bg-neutral-800 w-52 text-white text-center text-xs sm:text-base rounded-md shadow-md p-2 md:p-5 z-10">
       client
       <br /> component
     </div>
