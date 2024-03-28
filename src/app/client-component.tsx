eb78142c2381e8e2e6122f0cdcea3fa52fd77767
Daniel Ivanov <daniel.ivanov@me.com>
2024-03-28 22:46:14 +0100

    - typo;

--- src/app/client-component.tsx
+++ src/app/client-component.tsx
@@ -3,7 +3,7 @@
 export const ClientComponent: React.FC = () => {
   return (
     <div className="bg-neutral-800 w-52 text-white text-center text-xs sm:text-base rounded-md shadow-md p-2 md:p-5 z-10">
-      server
+      client
       <br /> component
     </div>
   );
